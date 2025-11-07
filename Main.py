c1b88e3cba04dfdc0c29d1c94966df4372684605
Gmoney48 <gael.m.mtz@gmail.com>
2025-11-07 14:40:37 +0000

    Created Folders

---
+++ Main.py
@@ -0,0 +1,15 @@
+# refactoring means to 
+# - improve the structure of existing code
+# - without changing its external behavior 
+# This helps enhance code readability 
+# maintainability, and efficiency 
+from problem_set_1 import problem1
+from advanced_slicing import advanced_slice
+from String_Methods import string_method
+from String_prop_Advanced_ops import SPAO
+problem1()
+advanced_slice()
+string_method()
+SPAO()
+
+
